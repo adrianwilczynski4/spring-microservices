0fda2a96f332eca1fcf16640b39778207ea77cf8
adrian.wilczynski <adrian.wilczynski@gmai.com>
2019-03-10 15:14:14 +0100

    First commit

---
+++ microapp/src/main/java/com/service/microapp/MicroappApplication.java
@@ -0,0 +1,13 @@
+package com.service.microapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroappApplication.class, args);
+	}
+
+}
